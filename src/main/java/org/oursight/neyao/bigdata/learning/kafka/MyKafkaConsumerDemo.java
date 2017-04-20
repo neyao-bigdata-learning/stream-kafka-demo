482870967c36b2c82196dfe00c0a6b0b1276efa8
姚能俊 <ynj@deepdata.com.cn>
2017-04-20 13:53:41 +0800

    log

--- src/main/java/org/oursight/neyao/bigdata/learning/kafka/MyKafkaConsumerDemo.java
+++ src/main/java/org/oursight/neyao/bigdata/learning/kafka/MyKafkaConsumerDemo.java
@@ -10,19 +10,20 @@ import java.util.List;
 import java.util.Properties;
 
 /**
+ * 注意：这个 group.id 不是随意填的，而是在<KAFKA>/config/consumer.properties中配置的
  * Created by neyao on 2017/4/6.
  */
 public class MyKafkaConsumerDemo {
 
     public static void main(String[] args) throws InterruptedException {
-//        manualOffset();
         autoOffset();
+//        manualOffset();
     }
     public static void manualOffset() throws InterruptedException {
         Properties props = new Properties();
-        props.put("bootstrap.servers", "192.168.0.200:30022");
-        props.put("group.id", "testgroup");
-        props.put("enable.auto.commit", "false");
+        props.put("bootstrap.servers", "192.168.0.200:30021");
+        props.put("group.id", "test-consumer-false");
+        props.put("enable.auto.commit", "fasle");
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
@@ -31,7 +32,7 @@ public class MyKafkaConsumerDemo {
         List<ConsumerRecord<String, String>> buffer = new ArrayList<>();
         while (true) {
             System.out.println("MyKafkaConsumerDemo.manualOffset, about to find records...");
-            ConsumerRecords<String, String> records = consumer.poll(100);
+            ConsumerRecords<String, String> records = consumer.poll(1);
             System.out.println("records: " + records);
 
             for (ConsumerRecord<String, String> record : records) {
@@ -49,11 +50,11 @@ public class MyKafkaConsumerDemo {
 
     public static void autoOffset() throws InterruptedException {
         Properties props = new Properties();
-        props.put("bootstrap.servers", "192.168.0.200:30022");
-        props.put("group.id", "consumer-group-test");
+        props.put("bootstrap.servers", "192.168.0.200:30021");
+        props.put("group.id", "test-consumer-group");
         props.put("enable.auto.commit", "true");
         props.put("auto.commit.interval.ms", "1000");
-//        props.put("auto.offset.reset", "earliest");
+        props.put("auto.offset.reset", "earliest");
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
@@ -62,7 +63,7 @@ public class MyKafkaConsumerDemo {
 
         while (true) {
             System.out.println("about to find records...");
-            ConsumerRecords<String, String> records = consumer.poll(100);
+            ConsumerRecords<String, String> records = consumer.poll(1);
 
             for (ConsumerRecord<String, String> record : records)
                 System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
--- src/main/java/org/oursight/neyao/bigdata/learning/kafka/MyKafkaProducerDemo.java
+++ src/main/java/org/oursight/neyao/bigdata/learning/kafka/MyKafkaProducerDemo.java
@@ -16,14 +16,16 @@ public class MyKafkaProducerDemo {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         Properties props = new Properties();
-        props.put("bootstrap.servers", "192.168.0.200:36053");
+        props.put("bootstrap.servers", "192.168.0.200:30021");
         props.put("acks", "all");
         props.put("retries", 0);
-        props.put("batch.size", 16384);
+//        props.put("batch.size", 16384);
+        props.put("batch.size", 0);
         props.put("linger.ms", 1);
         props.put("buffer.memory", 33554432);
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
 
         Producer<String, String> producer = new KafkaProducer<>(props);
         System.out.println("MyKafkaProducerDemo.main, about to send data from producer...");
@@ -32,7 +34,7 @@ public class MyKafkaProducerDemo {
             ProducerRecord<String, String> record = new ProducerRecord<String, String>("mytest-topic-2", testDataPrefix + Integer.toString(i), testDataPrefix + Integer.toString(i));
             Future<RecordMetadata> future = producer.send(record);
 //            Future<RecordMetadata> future = producer..send(record);
-            System.out.println(record + ", sent, result: " + future.get());
+            //System.out.println(record + ", sent, result: " + future.get());
         }
         producer.close();
     }
